0468b6eb549caa0e52fec2588aaf00a8549c9d4a
yzhwang <yzhwang@ucdavis.edu>
2013-07-10 13:57:11 -0400

    updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,24 @@
 var express = require('express');
 
+var fs = require('fs');
+var Buffer = require('buffer').Buffer;
+
+var buffer = new Buffer(100);
+
+fs.open("index.html", 'r', function(status, fd) {
+    if (status) {
+        console.log(status.message);
+        return;
+    }
+    fs.read(fd, buffer, 0, 100, 0, function(err, num) {
+        console.log(buffer.toString('utf-8', 0, num));
+    });
+});
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
